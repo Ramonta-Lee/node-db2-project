beee8e4a63ea20dfedbbb5c38df3d23a5a5544a0
unknown <viola4lfe@gmail.com>
2020-02-18 16:50:45 -0500

    Stretch: incorporated seed data

--- seeds/01-cars.js
+++ seeds/01-cars.js
@@ -1,13 +1,42 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').truncate()
-    .then(function () {
+  return knex("cars")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("cars").insert([
+        {
+          vin: "VIN001",
+          make: "Test1",
+          model: "Test01",
+          mileage: "1234",
+          "transmission type": "Automatic",
+          "title status": "Clean"
+        },
+        {
+          vin: "VIN002",
+          make: "Test2",
+          model: "Test02",
+          mileage: "12345",
+          "transmission type": "Automatic",
+          "title status": "Clean"
+        },
+        {
+          vin: "VIN003",
+          make: "Test3",
+          model: "Test03",
+          mileage: "123456",
+          "transmission type": "Manual",
+          "title status": "Salvage"
+        },
+        {
+          vin: "VIN004",
+          make: "Test4",
+          model: "Test04",
+          mileage: "1234567",
+          "transmission type": "Automatic",
+          "title status": "Wreck"
+        }
       ]);
     });
 };
