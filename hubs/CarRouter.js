464b913ee84ed75b125478ad80a95b0952ac2df0
unknown <viola4lfe@gmail.com>
2020-02-18 16:14:51 -0500

    working GET and POST command

--- hubs/CarRouter.js
+++ hubs/CarRouter.js
@@ -2,27 +2,45 @@ const express = require("express");
 
 const router = express.Router();
 
+const db = require("../data/dbConfig.js");
+
 router.get("/", (req, res) => {
-  db("fruits")
-    .then(fruits => {
-      res.json(fruits);
+  db.select("*")
+    .from("cars")
+    .then(cars => {
+      res.status(200).json(cars);
     })
     .catch(err => {
-      res.status(500).json({ message: "Failed to retrieve fruits" });
+      console.log(err);
+      res.status(500).json({ error: "Failed to retrieve cars" });
     });
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
-  db("fruits")
+  db("cars")
     .where({ id })
     .first()
-    .then(fruit => {
-      res.json(fruit);
+    .then(car => {
+      res.json(car);
     })
     .catch(err => {
-      res.status(500).json({ message: "Failed to retrieve fruit" });
+      console.log(err);
+      res.status(500).json({ message: "Failed to retrieve Car" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const carData = req.body;
+  db("cars")
+    .insert(carData)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      console.log("POST error", err);
+      res.status(500).json({ error: "Failed to store data" });
     });
 });
 
