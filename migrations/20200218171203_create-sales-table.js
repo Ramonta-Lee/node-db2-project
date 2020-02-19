ae76aace2f292397e0aee56b2926af2f98080047
unknown <viola4lfe@gmail.com>
2020-02-18 19:10:35 -0500

    playing around with the last stretch problem

--- migrations/20200218171203_create-sales-table.js
+++ migrations/20200218171203_create-sales-table.js
@@ -1,14 +1,17 @@
+// const CarTable = require("./20200218153237_create-car-dealer-table");
+
 exports.up = function(knex) {
   return knex.schema.createTable("sales", tbl => {
-    tbl.increments("id");
+    tbl.increments();
     tbl.integer("price");
     tbl
-      .foreign("id")
-      .references("id")
+      .boolean("is_sold")
+      // .foreign("id")
+      .references("sold")
       .inTable("cars");
   });
 };
 
 exports.down = function(knex) {
- return knex.schema.dropTableIfExists("sales");
+  return knex.schema.dropTableIfExists("sales");
 };
